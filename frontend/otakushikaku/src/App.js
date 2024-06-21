17820536991b037a78d84c6c1fab7b863d748a78
mrnobo09 <shahabkhanzada999@gmail.com>
2024-06-21 18:20:21 +0500

    Added Login Page

--- frontend/otakushikaku/src/App.js
+++ frontend/otakushikaku/src/App.js
@@ -1,10 +1,11 @@
 import {Routes,Route} from 'react-router-dom';
 import Home from './Screens/Home'
-
+import Login from './Screens/Authentication/Login'
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/login' element = {<Login/>}/>
     </Routes>
   )
 }
---
+++ frontend/otakushikaku/src/Screens/Authentication/Login.js
@@ -0,0 +1,9 @@
+import react from 'react'
+
+export default function Login(){
+    return(
+    <div class = "bg-[url('https://iili.io/dHMm9vS.jpg') w-[100vw] h-[100vh]">
+
+    </div>
+    );
+}
