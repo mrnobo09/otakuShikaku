c40b71653d3efea61e2d964d12e45d62e1aad012
mrnobo09 <shahabkhanzada999@gmail.com>
2024-06-22 23:24:47 +0500

    Improved Login Page

--- frontend/otakushikaku/src/Screens/Authentication/Login.js
+++ frontend/otakushikaku/src/Screens/Authentication/Login.js
@@ -2,8 +2,10 @@ import react from 'react'
 
 export default function Login(){
     return(
-    <div class = "bg-[url('https://iili.io/dHMm9vS.jpg') w-[100vw] h-[100vh]">
-
+    <div class = "bg-[url('https://iili.io/dHMm9vS.jpg')] bg-cover bg-center h-[100vh] grid justify-center items-center">
+        <div class = "bg-[#0f0f0f] w-[30vw] h-[70vh] rounded-[4%] grid justify-center">
+                <h1 class="text-[white] mt-3 text-[4vw] font-kanit">Login</h1>
+        </div>
     </div>
     );
 }
